9fb016bb9899e70d0c1cedbfcd962671ca760fbb
wpar8 <wpar8@SEASCAPE>
2023-06-17 19:53:38 -0500

    Add project files.

---
+++ Playground/Program.cs
@@ -0,0 +1,40 @@
+﻿string userInput;
+int parseOutput;
+bool succesfull = false;
+bool anotherAttempt = false;
+
+
+
+Console.WriteLine("We are going to try Parsing! \nPlease type a number.");
+userInput = Console.ReadLine();
+succesfull = Int32.TryParse(userInput, out parseOutput);
+anotherAttempt = check(succesfull, parseOutput);
+
+
+while (anotherAttempt)
+{
+    Console.WriteLine("Let's try this again. Type a number.");
+    userInput = Console.ReadLine();
+    succesfull = Int32.TryParse(userInput, out parseOutput);
+    anotherAttempt = check(succesfull, parseOutput);
+}
+
+
+static bool check(bool succesfull, int parsedText)
+{
+    if (succesfull)
+    {
+        Console.WriteLine($"Thanks for typing a number. Your number: {parsedText}");
+        Console.ReadLine();
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+
+}
+
+
+
